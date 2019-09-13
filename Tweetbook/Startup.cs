94938a16d094364dbcdf1b851efeb8b1bb5ee785
buibup <b5209194@gmail.com>
2019-09-14 01:58:08 +0700

    T3: Completed di-and-clean-service-registration

--- Tweetbook/Startup.cs
+++ Tweetbook/Startup.cs
@@ -15,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SwaggerOptions = Tweetbook.Options.SwaggerOptions;
 using Swashbuckle.AspNetCore.Swagger;
+using Tweetbook.Installers;
 
 namespace Tweetbook
 {
@@ -30,18 +31,7 @@ namespace Tweetbook
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<DataContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<DataContext>();
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddSwaggerGen(x =>
-            {
-                x.SwaggerDoc("v1", new Info { Title = "Tweetbook API", Version = "v1" });
-            });
+            services.InstallServicesInAssembly(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
