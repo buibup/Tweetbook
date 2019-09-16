c7727eaf5746e4a6bdf75dac1abc5fc3f8b0f21c
buibup <b5209194@gmail.com>
2019-09-16 23:44:44 +0700

    completed t6-updating-resources-with-put -> origin

--- Tweetbook/Contracts/V1/ApiRoutes.cs
+++ Tweetbook/Contracts/V1/ApiRoutes.cs
@@ -17,6 +17,10 @@ namespace Tweetbook.Contracts.V1
         {
             public const string GetAll = Base + "/posts";
 
+            public const string Update = Base + "/posts/{postId}";
+
+            public const string Delete = Base + "/posts/{postId}";
+
             public const string Get = Base + "/posts/{postId}";
 
             public const string Create = Base + "/posts";
